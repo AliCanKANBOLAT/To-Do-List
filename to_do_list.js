81c8a1a1df8166811d056b75268e2791f797db3e
Ali Can KANBOLAT <kanbolat.alican@gmail.com>
2023-12-19 21:35:48 +0300

    local-storage-arrangement-made

--- to_do_list.js
+++ to_do_list.js
@@ -4,14 +4,16 @@ const invisibleButton = document.querySelector('#invisibleButton');
 const clearButton = document.querySelector('#clearButton')
 const infoList = document.querySelector('#infoList')
 
+
 addButton.addEventListener('click', function () {
    let userValue = userInfo.value.trim();
   if (userValue !== '') {
     const liElement = document.createElement('li')
     liElement.innerHTML = userValue;
-    infoList.append(liElement);
-    liElement.classList.add('animate__animated', 'animate__bounceInLeft');
     userInfo.value ='';
+    infoList.append(liElement);
+    addToLocalStorage(liElement);
+    liElement.classList.add('animate__animated', 'animate__bounceInLeft');
     liElement.addEventListener("click", function () {
       this.style.textDecoration = this.style.textDecoration && this.style.textDecoration ===  'line-through' ? 'none' : 'line-through';
     })
@@ -36,5 +38,18 @@ addButton.addEventListener('click', function () {
 
 clearButton.addEventListener('click', function () {  
   infoList.innerHTML = '';
+  localStorage.removeItem('localItem');
   
 } )
+
+function addToLocalStorage(itemWhichAdd) {
+  let data = JSON.parse(localStorage.getItem('localItem')); //JSON.parse using for getting item from localstorage
+
+  if (data === null) {
+    localStorage.setItem('localItem', JSON.stringify(itemWhichAdd.innerHTML)); //JSON.stringify using to send to localstorage
+  } else {
+    data.push(itemWhichAdd.innerHTML);
+    localStorage.setItem('localItem', JSON.stringify(data));
+  }
+}
+  
