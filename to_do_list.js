d00738885848dfc54e79434da32ca5f359151ed2
Ali Can KANBOLAT <kanbolat.alican@gmail.com>
2023-12-20 18:27:19 +0300

    localstorage_added

--- to_do_list.js
+++ to_do_list.js
@@ -1,55 +1,64 @@
-const userInfo = document.querySelector('#inputText')
-const addButton = document.querySelector('#addButton')
+const userInfo = document.querySelector('#inputText');
+const addButton = document.querySelector('#addButton');
 const invisibleButton = document.querySelector('#invisibleButton');
-const clearButton = document.querySelector('#clearButton')
-const infoList = document.querySelector('#infoList')
+const clearButton = document.querySelector('#clearButton');
+const infoList = document.querySelector('#infoList');
 
+// Sayfa yüklendiğinde local storage'dan verileri çek
+const savedData = JSON.parse(localStorage.getItem('localItem')) || [];
+
+// localStorage'dan çekilen verileri sayfaya ekle
+savedData.forEach(item => {
+  createLiElement(item);
+});
 
 addButton.addEventListener('click', function () {
-   let userValue = userInfo.value.trim();
+  let userValue = userInfo.value.trim();
   if (userValue !== '') {
-    const liElement = document.createElement('li')
-    liElement.innerHTML = userValue;
-    userInfo.value ='';
-    infoList.append(liElement);
-    addToLocalStorage(liElement);
-    liElement.classList.add('animate__animated', 'animate__bounceInLeft');
-    liElement.addEventListener("click", function () {
-      this.style.textDecoration = this.style.textDecoration && this.style.textDecoration ===  'line-through' ? 'none' : 'line-through';
-    })
+    // Li elementini oluştur ve sayfaya ekle
+    createLiElement(userValue);
 
-    liElement.addEventListener("dblclick", function () {
-      
-      // Double click yapılan li elementini siliyoruz
-      liElement.remove();
-      
-    })
+    // local storage'a veriyi ekle
+    savedData.push(userValue);
+    localStorage.setItem('localItem', JSON.stringify(savedData));
 
-    invisibleButton.addEventListener("click", function() {
-      if (liElement.style.textDecoration === 'line-through')
-      liElement.classList.add('animate__animated', 'animate__hinge');
-    })
+    userInfo.value = '';
+  }
+});
 
-  }   
-  
-})
-
-
-
-clearButton.addEventListener('click', function () {  
+clearButton.addEventListener('click', function () {
+  // Tüm li elementlerini ve local storage'daki veriyi temizle
   infoList.innerHTML = '';
   localStorage.removeItem('localItem');
+  localStorage.clear();
+});
+
+function createLiElement(value) {
+  const liElement = document.createElement('li');
+  liElement.innerHTML = value;
+  infoList.append(liElement);
+  liElement.classList.add('animate__animated', 'animate__bounceInLeft');
   
-} )
+  liElement.addEventListener("click", function () {
+    // Li elementine tıklandığında stil değiştir
+    this.style.textDecoration = this.style.textDecoration && this.style.textDecoration === 'line-through' ? 'none' : 'line-through';
+  });
 
-function addToLocalStorage(itemWhichAdd) {
-  let data = JSON.parse(localStorage.getItem('localItem')); //JSON.parse using for getting item from localstorage
+  liElement.addEventListener("dblclick", function () {
+    // Double click yapılan li elementini siliyoruz
+    this.remove();
 
-  if (data === null) {
-    localStorage.setItem('localItem', JSON.stringify(itemWhichAdd.innerHTML)); //JSON.stringify using to send to localstorage
-  } else {
-    data.push(itemWhichAdd.innerHTML);
-    localStorage.setItem('localItem', JSON.stringify(data));
-  }
+    // local storage'dan da veriyi kaldırın
+    const index = savedData.indexOf(value);
+    if (index > -1) {
+      savedData.splice(index, 1);
+      localStorage.setItem('localItem', JSON.stringify(savedData));
+    }
+  });
+
+  invisibleButton.addEventListener("click", function () {
+    // invisibleButton'a tıklandığında stil değiştir
+    if (liElement.style.textDecoration === 'line-through')
+      liElement.classList.add('animate__animated', 'animate__hinge');
+  });
 }
-  
